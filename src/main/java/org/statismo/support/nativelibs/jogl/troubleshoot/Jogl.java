89abaeb004af6310afcd75d4df080486f7b2c595
Christoph Langguth <christoph.langguth@unibas.ch>
2014-12-02 16:15:01 +0100

    Updated JOGL tests to explicitly handle GL3

--- src/main/java/org/statismo/support/nativelibs/jogl/troubleshoot/Jogl.java
+++ src/main/java/org/statismo/support/nativelibs/jogl/troubleshoot/Jogl.java
@@ -1,7 +1,7 @@
 package org.statismo.support.nativelibs.jogl.troubleshoot;
 
 import com.jogamp.opengl.util.FPSAnimator;
-import org.statismo.support.nativelibs.NativeLibraryBundles;
+import org.statismo.support.nativelibs.jogl.troubleshoot.gl3.GL3Sample;
 
 import javax.media.opengl.*;
 import javax.media.opengl.awt.GLCanvas;
@@ -15,25 +15,6 @@ public class Jogl implements GLEventListener {
     private static final int FRAME_WIDTH = 300;
     private static final int FRAME_HEIGHT = 300;
     private static final int FRAME_PADDING = 50;
-
-    public static void main(String[] args) throws Exception {
-        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-        Map<String, GLProfile> profiles = GLProfiles.getAvailableProfiles(true);
-
-        int x = 0, y = 0;
-        for (Map.Entry<String, GLProfile> entry : profiles.entrySet()) {
-            new Jogl(entry.getKey(), entry.getValue(), x, y);
-            x += (FRAME_WIDTH + FRAME_PADDING);
-            if (x + FRAME_WIDTH > screenSize.width) {
-                x = 0;
-                y += FRAME_PADDING;
-                if (y + FRAME_HEIGHT <= screenSize.height) {
-                    y += FRAME_HEIGHT;
-                }
-            }
-        }
-    }
-
     private double theta = 0;
     private double s = 0;
     private double c = 0;
@@ -42,9 +23,9 @@ public class Jogl implements GLEventListener {
         GLCapabilities caps = new GLCapabilities(glp);
         GLCanvas canvas = new GLCanvas(caps);
 
-        Frame frame = new Frame(profileName);
+        Frame frame = new Frame("[GL2]" + profileName);
         frame.setSize(FRAME_WIDTH, FRAME_HEIGHT);
-        frame.setLocation(x,y);
+        frame.setLocation(x, y);
         frame.add(canvas);
         frame.setVisible(true);
 
@@ -58,6 +39,29 @@ public class Jogl implements GLEventListener {
 
         FPSAnimator animator = new FPSAnimator(canvas, 60);
         animator.start();
+    }
+
+    public static void main(String[] args) throws Exception {
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        Map<String, GLProfile> profiles = GLProfiles.getAvailableProfiles(true);
+
+        int x = 0, y = 0;
+        for (Map.Entry<String, GLProfile> entry : profiles.entrySet()) {
+            GLProfile profile = entry.getValue();
+            if (profile.isGL3()) {
+                new GL3Sample(entry.getKey(), profile, x, y, FRAME_WIDTH, FRAME_HEIGHT);
+            } else {
+                new Jogl(entry.getKey(), profile, x, y);
+            }
+            x += (FRAME_WIDTH + FRAME_PADDING);
+            if (x + FRAME_WIDTH > screenSize.width) {
+                x = 0;
+                y += FRAME_PADDING;
+                if (y + FRAME_HEIGHT <= screenSize.height) {
+                    y += FRAME_HEIGHT;
+                }
+            }
+        }
     }
 
     @Override
